2a95413c98331157b59e3d03a69936c9e2a92ce9
Ali Ahmad Abdullah <100586875+TRIPLE-A332@users.noreply.github.com>
2023-10-17 21:13:54 +0530

    Delete basic_operators.py

--- basic_operators.py
+++
@@ -1,6 +0,0 @@
-a=10
-b=2
-print ("addition of", a, "and", b, "=", a+b )
-print ("substraction of", a, "and", b, "=", a-b )
-print ("division of", a, "and", b, "=", a/b )
-print ("multiplication of", a, "and", b, "=", a*b )
