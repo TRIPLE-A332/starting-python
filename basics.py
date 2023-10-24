8de98cbb3e3459dbebe2b635049f2e22981c1cfe
Ali Ahmad Abdullah <100586875+TRIPLE-A332@users.noreply.github.com>
2023-10-24 17:14:14 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,47 @@
+# a=10
+# b=2
+# c="5"
+# print ("addition of", a, "and", b, "=", a+b )
+# print ("substraction of", a, "and", b, "=", a-b )
+# print ("division of", a, "and", b, "=", a/b )
+# print ("multiplication of", a, "and", b, "=", a*b )
+
+# c_numbers = int(c)
+# print("extrinsic type casting performed on", c_numbers + b)
+
+# #Taking Input
+# d= int(input("Enter your age "))
+# print("your age is", d)
+
+# e = input("initial letter of youe name ")
+# f = input("second letter of youe name ")
+# g = input("third letter of youe name ")
+# print("with concatenation", e+f+g)
+# print("without concatenation", e,f,g)
+
+# h=int(input("here you can only enter an int value"))
+# print("int value", h, "is printed")
+# print(int(h),int(c_numbers))
+
+# tuple1 = [("spring","summer"),('autumn', "winter")]
+# print(tuple1)
+# dict1 = {"age":19, "name":"Ali"}
+# print(dict1, "can vote")
+
+#STRINGS
+
+# print ("He said ,'i want to eat an apple\".")
+
+# print(
+#     """asnfa
+#     aasfnainnsdfa
+#     nnifka"""
+
+#     '''kya bhai
+#     nsidianf
+#     nsfnas
+#     asojmfo'''
+# )
+a = int(input("Enter your age "))
+b = input("your name ")
+print (a, b)
